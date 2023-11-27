82e05454aa3e3f3a49b6ad9ef2ece04471e9704d
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-11-27 20:51:21 +0100

    implement DateHeader composable

--- app/src/main/java/com/example/quoteoftheday/ui/favoritesscreen/FavoritesScreen.kt
+++ app/src/main/java/com/example/quoteoftheday/ui/favoritesscreen/FavoritesScreen.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.quoteoftheday.R
 import com.example.quoteoftheday.model.FavoriteQuote
+import java.time.Duration
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Composable
 fun FavoritesScreen(
@@ -31,7 +34,7 @@ fun FavoritesList(
     ) {
         grouped.forEach { (date, favoritesForDate) ->
             stickyHeader {
-                DateHeader(localDateTime = date)
+                DateHeader(date = date)
             }
             items(favoritesForDate) {
 
@@ -43,10 +46,23 @@ fun FavoritesList(
 
 @Composable
 fun DateHeader(
-    localDateTime: LocalDateTime,
+    date: LocalDateTime,
     modifier: Modifier = Modifier
 ) {
-
+    val today = LocalDateTime.now()
+    val duration = Duration.between(date, today)
+    val text = when {
+        duration.toDays() < 1L -> stringResource(id = R.string.today)
+        duration.toDays() in 1L..7L -> stringResource(id = R.string.this_week)
+        duration.toDays() < 30L -> stringResource(id = R.string.last_month)
+        duration.get(ChronoUnit.MONTHS) in 1L..2L -> stringResource(id = R.string.one_month)
+        duration.get(ChronoUnit.MONTHS) in 2L..6L -> stringResource(id = R.string.last_semester)
+        duration.get(ChronoUnit.MONTHS) in 7L..20L -> stringResource(id = R.string.one_year)
+        else -> stringResource(id = R.string.years_ago)
+    }
+    Surface {
+        Text(text = text)
+    }
 }
 
 @Composable
@@ -65,6 +81,12 @@ fun ItemMenu(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    /*var isSheetVisible by rememberSaveable {
+        mutableStateOf(false)
+    }
+    val toggleSheetVisibility = {
+        isSheetVisible = !isSheetVisible
+    }*/
     DropdownMenu(
         expanded = expanded,
         onDismissRequest = onDismiss) {
